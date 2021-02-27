8691c5946d1977ba4280b3124078d884fc0cf35b
confusedolive <jeronimomarbella@gmail.com>
2021-02-27 12:07:58 +0000

    fixed some spelling errors
    fixed some spelling errors

--- Code/Indexing.py
+++ Code/Indexing.py
@@ -4,7 +4,7 @@ from constants import INDEX_NAME , DOCU_TYPE
 
 es = Elasticsearch()
 #no need to specify host as we are doing it locally
-data_path = r'C:\Users\jeron\OneDrive\Desktop\university\CE306\Elastic_search\modified set.csv'
+data_path = r'local path for data to be indexed'
 
 def index_data():
     with open(data_path, encoding='utf8', mode='r') as f:
@@ -12,7 +12,6 @@ def index_data():
         helpers.bulk(es, reader,index=INDEX_NAME, doc_type=DOCU_TYPE)
     
 if __name__ == '__main__':
-    es.indices.delete(indx=INDEX_NAME)
     es.indices.create(index=INDEX_NAME, ignore=400)
     index_data()
 

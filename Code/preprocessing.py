184cbfa7e8359c61d5f1ab2d583165ad834873b7
confusedolive <69000323+confusedolive@users.noreply.github.com>
2021-02-27 12:10:08 +0000

    Update preprocessing.py

--- Code/preprocessing.py
+++ Code/preprocessing.py
@@ -6,11 +6,7 @@ df1 = df.copy()
 
 def clean_csv(df_1=df1):
     df_1.dropna(subset=['title','abstract'], inplace=True)
-#   df_1.dropna(subset=['title', 'abstract'], inplace=True)
     df_1.fillna('nan',inplace=True)
-#length of dataset is 107028, dropping missing values in columns title and abstract
-#filtering by articles written in 2020 and published by WHO
-#random sample of 5000 to ease the computing process
     df_1 = df_1.sample(n=5000)
     df_final = df_1[df_1['title'].apply(detect) == 'en']
 #filter choosing only articles which titles are at least in english 
